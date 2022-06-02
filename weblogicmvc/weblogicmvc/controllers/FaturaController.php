0a6baa5473fc424156d448f743c874c2bd8a81e6
Luis Ferreira <2211882@my.ipleiria.pt>
2022-06-02 16:31:36 +0100

    Criação de Views e Controladores

--- weblogicmvc/weblogicmvc/controllers/FaturaController.php
+++ weblogicmvc/weblogicmvc/controllers/FaturaController.php
@@ -1,18 +1,35 @@
 <?php
 
+//Igual ao Book
+
 	class FaturaController //extends BaseAuthController
 	{
 
-		public function index() {
+		public function index()
+        {
 
 			$this->makeView('fatura', 'index');
 
 		}
 
-		public function store() {
+		public function create()
+        {
+
 
 		}
+        public function store($idcliente)
+        {
+            //gravar Fatura( preencher todos os campos da fatura)
+            //redirect -> minhaFatura / create + id Fatura
+        }
 
+        /*
+        public  function selectclient//(filtro de pesquisa)
+        {
+
+        }
+
+        */
 	}
 
 
---
+++ weblogicmvc/weblogicmvc/controllers/LinhaFaturaController.php
@@ -0,0 +1,37 @@
+<?php
+
+
+// igual ao chaphters
+
+class LinhaFaturaController
+{
+
+    public function index()
+    {
+
+    }
+
+    public function create($idFatura)//,[$idProduto])
+    {
+
+    }
+
+    public function store($id)
+    {
+        //gravar linha de fautra*
+        //redirect -> linhaFatura/Create(idFatura)
+    }
+
+    public function edit($idLinhaFatura)
+    {
+
+    }
+
+
+
+    public function selectProduct($idFatura)
+    {
+
+    }
+
+}
